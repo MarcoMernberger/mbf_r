94b8bfaa027afe1703f38bb49e6ab30416de9218
finkernagel <finkernagel@imt.uni-marburg.de>
2019-04-11 14:04:58 +0000

    basic r python interface

---
+++ src/mbf_r/__init__.py
@@ -0,0 +1,38 @@
+# I can't figuer out how to do colnames(x) = value without a helper function
+import numpy as np
+import rpy2.robjects as ro
+import rpy2.robjects.numpy2ri
+import rpy2.rinterface as rinterface
+import rpy2.robjects.pandas2ri as pandas2ri
+
+import rpy2.robjects as ro
+
+#rpy2.robjects.numpy2ri.activate()
+#rpy2.robjects.pandas2ri.activate()
+
+
+def convert_dataframe_to_r(obj):
+    """Convert a Python DataFRame into int's R equivalent,
+
+    Stolen from pandas2ri, but I really don't want to activate the
+    automatic.
+    """
+    od = {}
+    for name, values in obj.iteritems():
+        try:
+            func = pandas2ri.py2rpy.registry[type(values)]
+            od[name] = func(values)
+        except Exception as e: #  pragma: no cover - defensive
+            raise ValueError('Error while trying to convert '
+                          'the column "%s". Fall back to string conversion. '
+                          'The error is: %s'
+                          % (name, str(e)))
+
+    return ro.vectors.DataFrame(od)
+ 
+
+
+def convert_dataframe_from_r(df_r):
+    """Take an R dataframe (with colnames and rownames) and turn it into pandas,
+    with a reset index."""
+    return pandas2ri.rpy2py_dataframe(df_r)
---
+++ tests/test_dataframe.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from pandas.testing import assert_frame_equal
+from mbf_r import convert_dataframe_to_r, convert_dataframe_from_r
+import rpy2.robjects as ro
+import pytest
+
+class TestDataFrameRoundTripping:
+
+
+    def test_simple_from(self):
+        rdf = ro.r("""data.frame(a=c(0.1, 0.2, 0.3))""")
+        pdf = convert_dataframe_from_r(rdf)
+        should = pd.DataFrame({'a': [0.1, 0.2, 0.3], 'idx': ['1','2','3']}).set_index('idx')
+        should.index.name = None
+        assert_frame_equal(pdf, should)
+
+    def test_simple(self):
+        df = pd.DataFrame({"a": [0.1, 0.2, 0.3]})
+        rdf = convert_dataframe_to_r(df)
+        pdf = convert_dataframe_from_r(rdf)
+        df.index = [str(x) for x in df.index]
+        assert_frame_equal(df, pdf)
+
+    def test_index_gets_dropped(self):
+        df = pd.DataFrame({"a": [0.1, 0.2, 0.3], 'idx':['a','b','c']}).set_index('idx')
+        rdf = convert_dataframe_to_r(df)
+        pdf = convert_dataframe_from_r(rdf)
+        df.index.name = None
+        assert_frame_equal(df, pdf)
+
+    def test_categorical(self):
+        df = pd.DataFrame({"a": pd.Categorical(['a','b','c', 'a'])})
+        rdf = convert_dataframe_to_r(df)
+        pdf = convert_dataframe_from_r(rdf)
+        df.index = [str(x) for x in df.index]
+        assert_frame_equal(df, pdf)
+
+    def test_raise_on_funny(self):
+        df = pd.DataFrame({"aaaa": [(1,2), (3,4)]})
+        with pytest.raises(ValueError) as e:
+            convert_dataframe_to_r(df)
+        assert 'aaaa' in str(e)
+
